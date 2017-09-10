feffdb734d628b9a1a5ad47115d8cfa04f17e063
isaito-kurumizawa <exkurumizawa@gmail.com>
2017-09-10 18:26:50 +0900

    サイズ変更

--- js/getimages.js
+++ js/getimages.js
@@ -26,7 +26,7 @@ function get()
             for (var i=0; i<data.resultCount; i++) {
                 // <a download="crop.mp4" href="uploaded/crop.mp4">
                 var insertTag = "<a href={0} target=\"_blank\"><li class=\"col-md-2 post\"><figure><img src=\"{1}\"><figcaption><h3>{2}</h3><span>{3}</span></figcaption></figure></li></a>";
-                insertTag = insertTag.replace("{0}",data.results[i].artworkUrl100.replace("100x100bb","500x500bb"))
+                insertTag = insertTag.replace("{0}",data.results[i].artworkUrl100.replace("100x100bb","1000x1000bb"))
                 insertTag = insertTag.replace("{1}",data.results[i].artworkUrl100.replace("100x100bb","300x300bb"))
                 insertTag = insertTag.replace("{2}",data.results[i].collectionName)
                 insertTag = insertTag.replace("{3}",data.results[i].artistName)
